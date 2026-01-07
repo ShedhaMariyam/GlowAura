768e23697933429d31c27a9e05445877877d56d0
Shedha Mariyam <mariyamshedha@gmail.com>
2026-01-07 16:00:31 +0530

    created a file for service layer

--- controllers/admin/category.Controller.js
+++ controllers/admin/category.Controller.js
@@ -100,7 +100,7 @@ const addCategory = async (req, res) => {
 
     return res
       .status(HTTP_STATUS.CREATED)
-      .json({ success: true, message: "Category Added Successfully" });
+      .json({ success: true, message: "Category Added Successfully", });
   } catch (error) {
     console.error("Add Category Error:", error);
     return res
